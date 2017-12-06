c8f85970f076a5fe2bfb987556c6ffeafa2dbb85
Mynor Xico <mynorottoniel.xico2@gmail.com>
2017-12-05 18:03:50 -0600

    Method for reading data from file

--- generatefeedvector.py
+++ generatefeedvector.py
@@ -9,10 +9,9 @@ def getwordcounts(url):
 
     # Loop over all the entries
     for e in d.entries:
-        if 'summary' in e: sumary = e.summary
+        if 'summary' in e: summary = e.summary
         else: summary = e.description
 
-        print(summary)
         # Exctract a list of words
         words = getwords(e.title + ' ' + summary)
         for word in words:
@@ -22,6 +21,38 @@ def getwordcounts(url):
 
 def getwords(html):
     # Remove all the HTML tags
-    txt = re.compile(r'<[^>] + >').sub('', html)
+    txt = re.compile(r'<[^>]+>').sub('', html)
 
     # Split word by all non-alpha characters
+    words = re.compile(r'[^A-Z^a-z]+').split(txt)
+
+    # Convert to lowercase
+    return [word.lower() for word in words if word != '']
+
+apcount = {}
+wordcounts = {}
+
+for feedurl in open('feedlist.txt'):
+    print(feedurl)
+    title, wc = getwordcounts(feedurl)
+    wordcounts[title] = wc
+    for word, count in wc.items():
+        apcount.setdefault(word, 0)
+        if count > 1:
+            apcount[word] += 2
+
+wordlist = []
+for w, bc in apcount.items():
+    frac = float(bc)/len(wordcounts)
+    if frac > 0.1 and frac < 0.5: wordlist.append(w)
+
+out = open('blogdata.txt', 'w')
+out.write('Blog')
+for word in wordlist: out.write('\t%s' %word)
+out.write('\n')
+for blog, wc in wordcounts.items():
+    out.write(blog)
+    for word in wordlist:
+        if word in wc: out.write('\t%d' % wc[word])
+        else: out.write('\t0')
+    out.write('\n')
